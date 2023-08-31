dbd2c91d8bbc3de154b7050eedacedfbf2ac2ca1
Emily Deng <emrdeng21@gmail.com>
2023-08-31 19:34:48 -0400

    Update

--- ajax.js
+++ ajax.js
@@ -2,9 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use('/dist', express.static(__dirname + '/dist'));  // for bundled js
-app.use('/images', express.static(__dirname + '/images'));  // for images
-app.use('/styles.css', express.static(__dirname + '/style.css'));  // for styles
+app.use(express.static(__dirname));
 
 app.get("/", function (req, res){
   res.sendFile(__dirname + "/index.html");
@@ -12,6 +10,6 @@ app.get("/", function (req, res){
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function(){
-  console.log(`Server is running on port ${PORT}.`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
