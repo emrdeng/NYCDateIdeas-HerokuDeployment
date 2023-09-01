ebc56865d55f7a47ef8bf49a6ff55e836f4d1996
Emily Deng <emrdeng21@gmail.com>
2023-08-31 22:22:18 -0400

    Update

--- ajax.js
+++ ajax.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fs = require('fs');  // This module allows you to read files
 
 const express = require("express");
 
@@ -10,18 +11,47 @@ app.get("/", function (req, res){
   res.sendFile(__dirname + "/index.html");
 })
 
+// app.get('/env-vars', (req, res) => {
+//   res.json({
+//     CUISINENAMESJSON: process.env.CUISINENAMESJSON,
+//     DATEACTIVITYBASEURL: process.env.DATEACTIVITYBASEURL,
+//     RESTBASEURL: process.env.RESTBASEURL,
+//     RESTAPIKEY: process.env.RESTAPIKEY,
+//     WEATHERURLCELSIUS: process.env.WEATHERURLCELSIUS,
+//     WEATHERURLFARENHEIT: process.env.WEATHERURLFARENHEIT,
+//     WEATHERFORECASTURLCELSIUS: process.env.WEATHERFORECASTURLCELSIUS,
+//     WEATHERFORECASTURLFARENHEIT: process.env.WEATHERFORECASTURLFARENHEIT,
+//   });
+// });
+
 app.get('/env-vars', (req, res) => {
-  res.json({
-    CUISINENAMESJSON: process.env.CUISINENAMESJSON,
-    DATEACTIVITYBASEURL: process.env.DATEACTIVITYBASEURL,
-    RESTBASEURL: process.env.RESTBASEURL,
-    RESTAPIKEY: process.env.RESTAPIKEY,
-    WEATHERURLCELSIUS: process.env.WEATHERURLCELSIUS,
-    WEATHERURLFARENHEIT: process.env.WEATHERURLFARENHEIT,
-    WEATHERFORECASTURLCELSIUS: process.env.WEATHERFORECASTURLCELSIUS,
-    WEATHERFORECASTURLFARENHEIT: process.env.WEATHERFORECASTURLFARENHEIT,
+  // Read the cuisineNames.json file
+  fs.readFile(process.env.CUISINENAMESJSON, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return res.status(500).json({ error: 'Failed to read the file' });
+    }
+    let cuisineData;
+    try {
+        cuisineData = JSON.parse(data);
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+        return res.status(500).json({ error: 'Failed to parse JSON' });
+    }
+    // Send the environment variables along with the parsed JSON data
+    res.json({
+        CUISINENAMESJSON: cuisineData, 
+        DATEACTIVITYBASEURL: process.env.DATEACTIVITYBASEURL,
+        RESTBASEURL: process.env.RESTBASEURL,
+        RESTAPIKEY: process.env.RESTAPIKEY,
+        WEATHERURLCELSIUS: process.env.WEATHERURLCELSIUS,
+        WEATHERURLFARENHEIT: process.env.WEATHERURLFARENHEIT,
+        WEATHERFORECASTURLCELSIUS: process.env.WEATHERFORECASTURLCELSIUS,
+        WEATHERFORECASTURLFARENHEIT: process.env.WEATHERFORECASTURLFARENHEIT,
+    });
   });
 });
+
 
 const PORT = process.env.PORT || 3000;
 
